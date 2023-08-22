443d082e07da48d68d49a1832be06ecc61311691
Kyle <kylemcguire35@gmail.com>
2023-08-21 19:06:30 -0700

    substitution file and test file completed

--- src/substitution.js
+++ src/substitution.js
@@ -6,8 +6,68 @@
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
+  //creating an alphabet array to reference
+  const alphabetArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+    const phrase = input.toLowerCase();
+    let phraseArray = [];
+    let codedPhrase = '';
+
+    //error handling
+    if (!alphabet) return false;
+
+    if (alphabet.length !== 26) return false;
+    
+    //check for duplicates
+    for (let i = 0; i < alphabet.length; i++) {
+      for (let j = i + 1; j < alphabet.length; j++) {
+        if (alphabet[i] === alphabet[j]) {
+          return false;
+        }
+      }
+    }
+
+    //create array from string
+    for (let i = 0; i < phrase.length; i++) {
+      phraseArray.push(phrase[i]);
+    }
+
+    if (encode) {
+      phraseArray.forEach((letter) => {
+        //check if the value is a letter in the alphabet
+        if (alphabetArr.includes(letter)) {
+
+          //find the index of the letter
+          let index = alphabetArr.indexOf(letter);
+          
+          //then combine the letter into the encoded string
+          codedPhrase += alphabet[index];
+
+        } else {
+          //if it's not a letter in the alphabet, just add to the string
+          codedPhrase += letter;
+        }
+      })
+    } else {
+      phraseArray.forEach((letter) => {
+        //check if the value is a letter in the alphabet
+        if (alphabet.includes(letter)) {
+
+          //find the index of the letter
+          let index = alphabet.indexOf(letter);
+          
+          //then combine the letter into the encoded string
+          codedPhrase += alphabetArr[index];
+
+        } else {
+          //if it's not a letter in the alphabet, just add to the string
+          codedPhrase += letter;
+        }
+      })
+    }
+    return codedPhrase;
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,45 @@
+const { expect } = require("chai");
+const { substitution } = require("../src/substitution")
+
 // Write your tests here!
+describe("substitution()", () => {
+    describe("error handling", () => {
+        it("should return false if the alphabet is not exactly 26 characters", () => {
+            const actual = substitution('test', 'shortalphabet');
+            expect(actual).to.be.false;
+        })
+
+        it("should return false if the alphabet contains duplicates", () => {
+            const actual = substitution('test', 'abcabcabcabcabcabcabcabcyz')
+            expect(actual).to.be.false;
+        })
+    })
+
+    describe("encoding", () => {
+        it("should encode a message given another alphabet, including one with special characters", () => {
+            const expected = 'y&ii$r&';
+            const actual = substitution('message', '$wae&zrdxtfcygvuhbijnokmpl');
+            expect(actual).to.equal(expected);
+        })
+
+        it("should maintain spaces in the message", () => {
+            const expected = 'elp xhm xf mbymwwmfj dne';
+            const actual = substitution('You are an excellent spy', 'xoyqmcgrukswaflnthdjpzibev');
+            expect(actual).to.equal(expected);
+        })
+
+        it("should ignore capitalized letters", () => {
+            const expected = 'y&ii$r&';
+            const actual = substitution('MesSage', "$wae&zrdxtfcygvuhbijnokmpl");
+            expect(actual).to.equal(expected);
+        })
+    })
+
+    describe("decoding", () => {
+        it("should encode a message given another alphabet, including one with special characters", () => {
+            const expected = 'message';
+            const actual = substitution('y&ii$r&', '$wae&zrdxtfcygvuhbijnokmpl', false);
+            expect(actual).to.equal(expected);
+        })
+    })
+})
